326eb22c9629c8d53be7cc9cfd4db361e162c3a5
Steven prince <unknownpersonnel59@gmail.com>
2025-05-06 21:17:07 +0100

    Update Withdraw.tsx

--- src/pages/Withdraw.tsx
+++ src/pages/Withdraw.tsx
@@ -73,7 +73,7 @@ const Withdraw = () => {
     }
 
     // Validate BPC code
-    if (bpcCode !== "BPC816222") {
+    if (bpcCode !== "BPCOKAZI2005@#") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
