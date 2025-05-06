dbe3d4eea35681e34a53fb6293e6edec15f69e2e
Steven prince <unknownpersonnel59@gmail.com>
2025-05-06 20:16:55 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -49,7 +49,7 @@ const BuyBPCPayment = () => {
       </div>
 
       <div className="flex flex-col items-center p-6 mb-6">
-        <h1 className="text-4xl font-bold mb-2">NGN 6,200</h1>
+        <h1 className="text-4xl font-bold mb-2">NGN 6,500</h1>
         <p className="text-gray-600">BPC Code Purchase</p>
       </div>
 
@@ -67,11 +67,11 @@ const BuyBPCPayment = () => {
         <div className="mb-4">
           <p className="text-gray-500 text-sm">Amount</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">NGN 6200</p>
+            <p className="text-2xl font-bold">NGN 6500</p>
             <Button 
               variant="default" 
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("6200", "Amount")}
+              onClick={() => handleCopy("6500", "Amount")}
             >
               <Copy size={18} />
               Copy
@@ -82,11 +82,11 @@ const BuyBPCPayment = () => {
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">6957666738</p>
+            <p className="text-2xl font-bold">6103154535</p>
             <Button 
               variant="default" 
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("6957666738", "Account Number")}
+              onClick={() => handleCopy("6103154535", "Account Number")}
             >
               <Copy size={18} />
               Copy
@@ -96,12 +96,12 @@ const BuyBPCPayment = () => {
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Bank Name</p>
-          <p className="text-2xl font-bold">MONIEPOINT MFB</p>
+          <p className="text-2xl font-bold">OPAY</p>
         </div>
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Name</p>
-          <p className="text-2xl font-bold">LIBERTY CHINEMEREM</p>
+          <p className="text-2xl font-bold">GODWIN WILLIAMS FRIDAY</p>
         </div>
       </div>
 
